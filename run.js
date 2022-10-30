bb54a91d436805284760e8c2fa836c931fe9659e
Manasi <manasi@openbrix.co.uk>
2022-10-30 19:57:06 +0530

    Assignment

--- run.js
+++ run.js
@@ -4,9 +4,6 @@ const flows = require("./flows");
 let length = flowNumbers.lengthOfFlows(flows.flows);
 console.log ("length of flows: ",length);
 
-let flowsWithmoreThan2Tenants = flowNumbers.moreThan2Tenants(flows.flows);
-console.log ("flows with more than 2 tenants: ", flowsWithmoreThan2Tenants);
-
 let val = flowNumbers.toCheck(flows.flows,"is_deleted");
 console.log ("number of deleted flows: ", val);
 
@@ -15,6 +12,9 @@ console.log ("number of completed flows: ", val);
 
 val = flowNumbers.toCheck(flows.flows,"is_archived");
 console.log ("number of archived flows: ", val);
+
+val = flowNumbers.moreThan2Tenants(flows.flows);
+console.log ("number of flows with more than 2 tenants: ", val);
 
 val = flowNumbers.toCheck(flows.flows,"is_ast_generated");
 console.log ("number of ast generated flows: ", val);
@@ -30,6 +30,29 @@ console.log ("number of flows created from reapit: ", val);
 
 val = flowNumbers.toCheck(flows.flows,"is_added_to_property");
 console.log ("number of flows added to proparty: ", val);
+
+val = flowNumbers.compareAndCheck(flows.flows, "landlord_id", "===", null);
+console.log ("number of flows where landlords are empty : ", val);
+
+val = flowNumbers.compareAndCheck(flows.flows, "is_ast_signed", "!==", false);
+console.log ("number of flows where ast is generated and its signed : ", val);
+
+val = flowNumbers.compareAndCheck(flows.flows, "is_ast_signed", "===", false);
+console.log ("number of flows where ast is generated and its not signed : ", val);
+
+val = flowNumbers.rentalAmount (flows.flows);
+console.log ("number of flows where rental amount is greater than 5000 : ", val);
+
+
+
+
+
+
+
+
+
+
+
 
 
 
