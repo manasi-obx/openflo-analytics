762071aaa3f2367bbd8fece72007db5ea64fd1e3
Manasi <manasi@openbrix.co.uk>
2022-11-01 08:06:36 +0530

    Completed

--- run.js
+++ run.js
@@ -43,6 +43,37 @@ console.log ("number of flows where ast is generated and its not signed : ", val
 val = flowNumbers.rentalAmount (flows.flows);
 console.log ("number of flows where rental amount is greater than 5000 : ", val);
 
+val = flowNumbers.irpRequired (flows.flows);
+console.log ("number of flows where IRP is required: ", val);
+
+val = flowNumbers.irpNotRequired (flows.flows);
+console.log ("number of flows where IRP is not required: ", val);
+
+val = flowNumbers.tenantWaterResoncibility (flows.flows);
+console.log ("number of flows where tenant has the water responsibility: ", val);
+
+val = flowNumbers.agentWaterResoncibility (flows.flows);
+console.log ("number of flows where agent has the water responsibility: ", val);
+
+val = flowNumbers.furnishedProperty (flows.flows);
+console.log ("number of flows with furnished property: ", val);
+
+val = flowNumbers.certificates (flows.flows);
+console.log ("number of flows where number of certificates is greater than 2: ", val);
+
+val = flowNumbers.average(flows.flows, "open_flow_rental_offer", "rental_amount");
+console.log ("average rental amount per flow: ", val);
+
+val = flowNumbers.average(flows.flows, "open_flow_holding_deposit", "holding_desposit_amount");
+console.log ("average holding deposit per flow: ", val);
+
+val = flowNumbers.valPerFlow(flows.flows, "open_flow_certificates");
+console.log ("number of certificates per flow: ", val);
+
+val = flowNumbers.valPerFlow(flows.flows, "tenants");
+console.log ("number of tenants per flow: ", val);
+
+
 
 
 
