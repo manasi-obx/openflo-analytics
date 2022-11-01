97d9decfa9e127bcac100b5c41e2be162c8f60fa
Manasi <manasi@openbrix.co.uk>
2022-11-01 08:07:39 +0530

    completed

--- analytics/overAllFlowNumbers.js
+++ analytics/overAllFlowNumbers.js
@@ -41,11 +41,103 @@ function compareAndCheck (flows, prop, op, val) {
 }
 
 function rentalAmount (flows) {
-    let valToCheck = flows.filter(flow => {
-       return flow[open_flow_rental_offer[rental_amount]] > 5000;
+    let tenants = flows.filter(flow => {
+        if (flow.open_flow_rental_offer){
+            return flow.open_flow_rental_offer.rental_amount > 5000;
+        } else {
+            return false;
+        }
     })
-    return valToCheck.length;
+    return tenants.length;
 }
+
+function irpRequired (flows) {
+    let tenants = flows.filter(flow => {
+        if (flow.open_flow_rental_offer){
+            return (flow.open_flow_rental_offer.is_irp_required);
+        } else {
+            return false;
+        }
+    })
+    return tenants.length;
+}
+
+function irpNotRequired (flows) {
+    let tenants = flows.filter(flow => {
+        if (flow.open_flow_rental_offer){
+            return !flow.open_flow_rental_offer.is_irp_required;
+        } else {
+            return false;
+        }
+    })
+    return tenants.length;
+}
+
+function tenantWaterResoncibility (flows) {
+    let tenants = flows.filter(flow => {
+        if (flow.open_flow_rental_offer){
+            return flow.open_flow_rental_offer.tenant_water_responsibility === true;
+        } else {
+            return false;
+        }
+    })
+    return tenants.length;
+}
+
+
+function agentWaterResoncibility (flows) {
+    let tenants = flows.filter(flow => {
+        if (flow.open_flow_rental_offer){
+            return (flow.open_flow_rental_offer.tenant_water_responsibility === false);
+        } else {
+            return false;
+        }
+    })
+    return tenants.length;
+}
+
+function furnishedProperty (flows) {
+    let property = flows.filter(flow => {
+        if (flow.open_flow_rental_offer){
+            return (flow.open_flow_rental_offer.furnished);
+        } else {
+            return false;
+        }
+    })
+    return property.length;
+}
+
+function certificates (flows) {
+    let cartificate = flows.filter(flow =>{
+        return flow.open_flow_certificates.length > 2;
+    })
+    return cartificate.length;
+}
+
+function average(flows, prop, val) {
+    let sum = 0;
+    flows.map(flow => {
+        if (flow[prop]) {
+            let value = flow[prop][val];
+            sum += value;
+        }   
+    })
+    let avg =  sum / flows.length
+    return avg.toFixed(2)
+}
+
+function valPerFlow (flows, prop) {
+    let sum =0;
+        let flowsForProp = flows.filter(flow => {
+            if(flow[prop]) {
+                let value = flow[prop].length;
+                return sum += value;
+            }
+        })
+        let avg = sum /flows.length
+        return avg.toFixed(2)
+}
+
 
 
 module.exports = {
@@ -53,5 +145,13 @@ module.exports = {
     moreThan2Tenants,
     toCheck, 
     compareAndCheck,
-    rentalAmount
+    rentalAmount,
+    irpRequired,
+    irpNotRequired,
+    tenantWaterResoncibility,
+    agentWaterResoncibility,
+    furnishedProperty,
+    certificates,
+    average,
+    valPerFlow
 }
